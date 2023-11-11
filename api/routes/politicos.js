2747826948e69b017a5cd12acdab006be002d11b
josusz <josuhcarvalho@gmail.com>
2023-11-11 17:30:14 -0300

    correção, ainda sem token: validação, métodos post e put

--- api/routes/politicos.js
+++ api/routes/politicos.js
@@ -1,9 +1,25 @@
 import express from 'express';
 import { connectToDatabase} from '../utils/mongodb.js';
+import { check, validationResult } from 'express-validator'
 
 const router = express.Router()
 const {db, ObjectId} = await connectToDatabase()
 const nomeCollection = 'politicos'
+
+const validaPolitico = [
+    check('nome')
+    .not().isEmpty().trim().withMessage('É obrigatório informar o nome'),
+    check('partido')
+    .not().isEmpty().trim().withMessage('É obrigatório informar o partido'),
+    check('cargo')
+    .not().isEmpty().trim().withMessage('É obrigatório informar o cargo'),
+    check('data_filiação')
+    .not().isEmpty().trim().withMessage('É obrigatório informar a data de filiação'),
+    check('valor_ajuda_mensal')
+    .isNumeric().withMessage('O valor da ajuda mensal deve ser um número'),
+    check('numerourna')
+    .isNumeric().withMessage('O número da urna deve ser um número'),
+]
 
 /**
  * GET /api/politicos
@@ -30,7 +46,7 @@ router.get('/', async(req, res) => {
 
 /**
  * GET /api/prestadores/id/:id
- * Lista todos os prestadores de serviço
+ * Lista um político pelo id
  */
 router.get('/id/:id', async(req, res)=> {
     try{
@@ -49,13 +65,52 @@ router.get('/id/:id', async(req, res)=> {
 
 /**
  * DELETE /api/politicos/:id
- * Apaga o prestador de serviço pelo id
+ * Apaga o político pelo id
  */
 router.delete('/:id', async(req, res) => {
     await db.collection(nomeCollection)
     .deleteOne({"_id": { $eq: ObjectId(req.params.id)}})
     .then(result => res.status(200).send(result))
     .catch(err => res.status(400).json(err))
+})
+
+/**
+ * POST /api/politicos
+ * Insere um novo político
+ */
+router.post('/', validaPolitico, async(req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        return res.status(400).json(({
+            errors: errors.array()
+        }))
+    } else {
+        await db.collection(nomeCollection)
+        .insertOne(req.body)
+        .then(result => res.status(200).send(result))
+        .catch(err => res.status(400).json(err))
+    }
+})
+
+/**
+ * PUT /api/politicos
+ * Altera um político
+ */
+router.put('/', validaPolitico, async(req, res) => {
+    let idDocumento = req.body._id //armazenando o id do documento
+    delete req.body._id //iremos remover o id do body
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        return res.status(400).json(({
+            errors: errors.array()
+        }))
+    } else {
+        await db.collection(nomeCollection)
+        .updateOne({'_id': {$eq : ObjectId(idDocumento)}},
+                   { $set: req.body})
+        .then(result => res.status(200).send(result))
+        .catch(err => res.status(400).json(err))
+    }
 })
 
 export default router
